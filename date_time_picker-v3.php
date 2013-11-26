15a69721c5218cc8ac09b9028f07e3f4f9e0abec
soderlind <per@soderlind.no>
2013-11-26 17:23:41 +0100

    fixed syntax error

--- date_time_picker-v3.php
+++ date_time_picker-v3.php
@@ -28,7 +28,7 @@ class acf_field_date_time_picker extends acf_Field {
 		$this->title = __( 'Date and Time Picker' );
 		$this->domain = 'acf-date_time_picker';
 		$this->defaults = array(
-			, 'label'              => __( 'Choose Time', $this->domain )
+			  'label'              => __( 'Choose Time', $this->domain )
 			, 'time_format'        => 'hh:mm'
 			, 'show_date'          => 'true'
 			, 'date_format'        => 'yy-mm-dd'
