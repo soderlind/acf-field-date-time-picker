4a27791e3a16aa835b36a38fc0b24675c3bd0c99
soderlind <per@soderlind.no>
2014-09-23 22:40:01 +0200

    Increase tested version to 4.0

--- acf-date_time_picker.php
+++ acf-date_time_picker.php
@@ -3,7 +3,7 @@
 Plugin Name: Advanced Custom Fields: Date and Time Picker
 Plugin URI: https://github.com/soderlind/acf-field-date-time-picker
 Description: Date and Time Picker field for Advanced Custom Fields
-Version: 2.0.18
+Version: 2.0.18.1
 Author: Per Soderlind
 Author URI: http://soderlind.no
 License: GPLv2 or later
--- readme.txt
+++ readme.txt
@@ -3,8 +3,8 @@ Contributors: PerS
 Donate link: http://soderlind.no/donate/
 Tags: acf, custom field,datepicker,timepicker
 Requires at least: 3.6
-Tested up to: 3.9
-Stable tag: 2.0.18
+Tested up to: 4.0
+Stable tag: 2.0.18.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
